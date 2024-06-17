b8d331546eaa744a870de33d7dfe8f461d0cc162
AnhMinhTruongHoang <truonghoanganhminh2000@gmail.com>
2024-06-18 01:13:07 +0700

    ok

--- src/component.js/Mycomponent.js
+++ src/component.js/Mycomponent.js
@@ -1,38 +1,27 @@
 import React from "react";
+import UserInfor from "./Userinfor";
 
 class Mycomponent extends React.Component {
-  state = {
-    name: "Minh",
-    address: "Dn",
-    age: 24,
-  };
+  
 
-  Onclickhandler = (event) => {
-    this.setState({
-      name: "eric",
-      age: Math.floor(Math.random() * 100 + 1),
-    });
+  // Onclickhandler = (event) => {
+  //   this.setState({
+  //     name: "eric",
+  //     age: Math.floor(Math.random() * 100 + 1),
+  //   });
 
-    alert("ok");
-  };
+  //   alert("ok");
+  // };
 
-  onMouseoverHander = (event) => {
-    console.log(event.pageX);
-  };
-
+  // onMouseoverHander = (event) => {
+  //   console.log(event.pageX);
+  // };
+ 
   render() {
     return (
       <div>
-        my name is {this.state.name} and im from {this.state.address} im
-        {this.state.age}
-        <button onClick={this.Onclickhandler}>click me</button>
-        <button
-          onMouseOver={(event) => {
-            this.onMouseoverHander(event);
-          }}
-        >
-          hover
-        </button>
+     
+        <UserInfor/>
       </div>
     );
   }
---
+++ src/component.js/Userinfor.js
@@ -0,0 +1,64 @@
+import React from "react";
+
+class UserInfor extends React.Component {
+  // Khởi tạo state của component với các thuộc tính name, address và age
+  state = {
+    name: "Minh",
+    address: "Dn",
+    age: 24,
+  };
+
+  // Hàm xử lý để cập nhật name trong state
+  handerOnChange = (event) => {
+    this.setState({
+      name: event.target.value,
+    });
+  };
+
+  // Hàm xử lý để cập nhật age trong state
+  handerOnChange2 = (event) => {
+    this.setState({
+      age: event.target.value,
+    });
+  };
+
+  // Hàm xử lý khi form được submit để ngăn hành động mặc định và hiển thị thông báo với name
+  handerOnSubmit = (event) => {
+    event.preventDefault();
+    alert(`${this.state.name} & ${this.state.age}`);
+  };
+
+  // Hàm render để hiển thị component
+  render() {
+    return (
+      <div>
+        {/* Hiển thị giá trị hiện tại của state */}
+        tên tôi là {this.state.name} và tôi đến từ {this.state.address}, tôi{" "}
+        {this.state.age} tuổi
+        {/* Form để cập nhật name và age */}
+        <form onSubmit={(event) => this.handerOnSubmit(event)}>
+          <label>Tên của bạn:</label>
+          {/* Ô input cho name với hàm xử lý thay đổi */}
+          <input
+            value={this.state.name}
+            type="text"
+            onChange={(event) => this.handerOnChange(event)}
+          ></input>
+
+          <label>Tuổi của bạn:</label>
+          {/* Ô input cho age với hàm xử lý thay đổi */}
+          <input
+            value={this.state.age}
+            type="number"
+            onChange={(event) => this.handerOnChange2(event)}
+          ></input>
+
+          {/* Nút submit */}
+          <button>Submit</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default UserInfor;

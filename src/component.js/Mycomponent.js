00b2f79c5eba74e826b25c5b597819b919b1aa3d
AnhMinhTruongHoang <truonghoanganhminh2000@gmail.com>
2024-06-17 23:51:31 +0700

    ok

--- src/component.js/Mycomponent.js
+++ src/component.js/Mycomponent.js
@@ -1,20 +1,41 @@
 import React from "react";
 
 class Mycomponent extends React.Component {
-  
-    state = {
+  state = {
     name: "Minh",
     address: "Dn",
     age: 24,
   };
 
-  render() {
-    return ( 
-           
-        <div> my name is {this.state.name} and im from {this.state.address} </div>
+  Onclickhandler = (event) => {
+    this.setState({
+      name: "eric",
+      age: Math.floor(Math.random() * 100 + 1),
+    });
 
+    alert("ok");
+  };
+
+  onMouseoverHander = (event) => {
+    console.log(event.pageX);
+  };
+
+  render() {
+    return (
+      <div>
+        my name is {this.state.name} and im from {this.state.address} im
+        {this.state.age}
+        <button onClick={this.Onclickhandler}>click me</button>
+        <button
+          onMouseOver={(event) => {
+            this.onMouseoverHander(event);
+          }}
+        >
+          hover
+        </button>
+      </div>
     );
   }
-};
+}
 
 export default Mycomponent;

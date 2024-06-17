878e54b46ab4984b3d7ba38b18b7bd07af25d079
AnhMinhTruongHoang <truonghoanganhminh2000@gmail.com>
2024-06-17 15:40:55 +0700

    ok

---
+++ src/component.js/Mycomponent.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+class Mycomponent extends React.Component{
+    
+    render() {
+        
+        return (
+            <div> my first Component</div>
+        );
+    }
+};
+
+export default Mycomponent;

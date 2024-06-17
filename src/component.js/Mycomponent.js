c23737f14f9397ab738760faaea104c568098350
AnhMinhTruongHoang <truonghoanganhminh2000@gmail.com>
2024-06-17 15:52:00 +0700

    ok

--- src/component.js/Mycomponent.js
+++ src/component.js/Mycomponent.js
@@ -1,14 +1,20 @@
 import React from "react";
 
+class Mycomponent extends React.Component {
+  
+    state = {
+    name: "Minh",
+    address: "Dn",
+    age: 24,
+  };
 
-class Mycomponent extends React.Component{
-    
-    render() {
-        
-        return (
-            <div> my first Component</div>
-        );
-    }
+  render() {
+    return ( 
+           
+        <div> my name is {this.state.name} and im from {this.state.address} </div>
+
+    );
+  }
 };
 
 export default Mycomponent;

fa0c98fd14e6f8738aef7a6df386e42f8290b888
AnhMinhTruongHoang <truonghoanganhminh2000@gmail.com>
2024-08-05 01:34:18 +0700

    ok

--- src/component.js/Users/DetailQuiz.js
+++ src/component.js/Users/DetailQuiz.js
@@ -1,65 +1,92 @@
-import { useEffect, useState } from "react";
-import { useParams, useLocation } from "react-router-dom";
-import { getDataQuiz } from "../../Service/apiServices";
-import _ from "lodash";
-import "./Quiz.scss";
-import Question from "./Questions";
+import { useEffect, useState } from "react"; // Import các hook useEffect và useState từ React
+import { useParams, useLocation } from "react-router-dom"; // Import các hook useParams và useLocation từ react-router-dom
+import { getDataQuiz } from "../../Service/apiServices"; // Import hàm getDataQuiz từ dịch vụ API
+import _ from "lodash"; // Import thư viện lodash
+import "./Quiz.scss"; // Import file CSS để áp dụng style
+import Question from "./Questions"; // Import component Question
 
 // Định nghĩa component DetailQuiz
 const DetailQuiz = (props) => {
   // Sử dụng hook useParams để lấy tham số từ URL
   const params = useParams();
   const quizId = params.id; // Lấy giá trị của tham số id từ URL
-  const location = useLocation();
-  const [dataQuiz, setDataQuiz] = useState([]);
-  const [index, setIndex] = useState(0);
-  // Sử dụng hook useEffect để thực hiện một số hành động khi component được render hoặc khi quizId thay đổi
+  const location = useLocation(); // Sử dụng hook useLocation để lấy thông tin về vị trí hiện tại
+  const [dataQuiz, setDataQuiz] = useState([]); // Khai báo state để lưu trữ dữ liệu câu hỏi
+  const [index, setIndex] = useState(0); // Khai báo state để lưu trữ chỉ số câu hỏi hiện tại
 
+  // Sử dụng hook useEffect để thực hiện một số hành động khi component được render hoặc khi quizId thay đổi
   useEffect(() => {
     fetchQuestions(); // Gọi hàm fetchQuestions khi component được render hoặc quizId thay đổi
-  }, [quizId]); // Mảng phụ thuộc bao gồm quizId
+  },[quizId]); // Mảng phụ thuộc bao gồm quizId
 
   // Định nghĩa hàm fetchQuestions để lấy dữ liệu câu hỏi từ API
   const fetchQuestions = async () => {
     let res = await getDataQuiz(quizId); // Gọi hàm getDataQuiz từ apiServices với tham số quizId
     console.log("check Questions:", res); // In ra console kết quả trả về từ API
     if (res && res.EC === 0) {
-      let raw = res.DT;
+      // Kiểm tra nếu kết quả trả về thành công
+      let raw = res.DT; // Lấy dữ liệu thô từ kết quả trả về
       let data = _.chain(raw)
-        // Group the elements of Array based on `color` property
+        // Nhóm các phần tử của mảng dựa trên thuộc tính `id`
         .groupBy("id")
-        // `key` is group's name (color), `value` is the array of objects
+        // `key` là tên của nhóm (màu sắc), `value` là mảng các đối tượng
         .map((value, key) => {
-          let answers = [];
+          let answers = []; // Khởi tạo mảng câu trả lời
           let questionDescription,
-            image = null;
+            image = null; // Khởi tạo các biến để lưu trữ mô tả câu hỏi và ảnh
 
           value.forEach((item, index) => {
+            // Lặp qua từng phần tử trong mảng
             if (index === 0) {
+              // Lấy mô tả câu hỏi và ảnh từ phần tử đầu tiên
               questionDescription = item.description;
               image = item.image;
             }
-            answers.push(item.answers);
+            item.answers.isSelected = false
+            answers.push(item.answers); // Thêm câu trả lời vào mảng câu trả lời
           });
 
+          // Trả về đối tượng chứa id câu hỏi, câu trả lời, mô tả câu hỏi và ảnh
           return { questionId: key, answers, questionDescription, image };
         })
-
-        .value();
-      console.log(data);
-      setDataQuiz(data);
+        .value(); // Kết thúc chuỗi xử lý và trả về mảng dữ liệu
+      console.log(data); // In ra console mảng dữ liệu đã xử lý
+      setDataQuiz(data); // Cập nhật state dataQuiz với mảng dữ liệu đã xử lý
     }
   };
 
-  console.log("check data quiz", dataQuiz);
+  console.log("check data quiz", dataQuiz); // In ra console dữ liệu câu hỏi
 
+  // Định nghĩa hàm handlePrev để xử lý khi nhấn nút Prev
   const handlePrev = () => {
-    if (index - 1 < 0) return;
-    setIndex(index - 1);
+    if (index - 1 < 0) return; // Nếu chỉ số câu hỏi hiện tại là 0 thì không làm gì cả
+    setIndex(index - 1); // Giảm chỉ số câu hỏi hiện tại đi 1
   };
 
+  const handleCheckBox2 = (answersId, questionId) => {
+      let dataQuizClone = _.cloneDeep(dataQuiz)
+      let question = dataQuizClone.find(item => +item.questionId === +questionId)
+      if(question && question.answers){
+        question.answers = question.answers.map(item =>{
+          if(+item.id === +answersId){
+              item.isSelected = !item.isSelected;
+          }
+          return item;
+        
+        })
+       
+      }
+      let index = dataQuizClone.findIndex(item => +item.questionId === +questionId)
+      if(index > -1){
+        dataQuizClone[index] = question;
+        setDataQuiz(dataQuizClone);
+      }
+      
+    }
+
+  // Định nghĩa hàm handleNext để xử lý khi nhấn nút Next
   const handleNext = () => {
-    if (dataQuiz && dataQuiz.length > index + 1) setIndex(index + 1);
+    if (dataQuiz && dataQuiz.length > index + 1) setIndex(index + 1); // Tăng chỉ số câu hỏi hiện tại lên 1 nếu chưa đến câu hỏi cuối cùng
   };
 
   // JSX để render giao diện của component
@@ -67,28 +94,32 @@ const DetailQuiz = (props) => {
     <div className="detail-quiz-container">
       <div className="left-content">
         <div className="title"></div>
-        {location?.state?.quizTitle}
+        {location?.state?.quizTitle} {/* Hiển thị tiêu đề của bài quiz */}
         <hr style={{ color: "red" }} />
         <div className="q-body">
-          <img />
+          <img  /> {/* Hiển thị ảnh */}
         </div>
         <div className="q-content">
           <Question
+            handleCheckBox2={handleCheckBox2}
             index={index}
-            data={dataQuiz && dataQuiz.length > 0 ? dataQuiz[index] : []}
+            data={dataQuiz && dataQuiz.length > 0 ? dataQuiz[index] : []} // Truyền dữ liệu câu hỏi vào component Question
           />
         </div>
         <div className="footer">
           <button className="btn btn-secondary" onClick={() => handlePrev()}>
-            Prev
+            Prev {/* Nút Prev để chuyển về câu hỏi trước */}
           </button>
           <button className="btn btn-primary " onClick={() => handleNext()}>
-            Next
+            Next {/* Nút Next để chuyển đến câu hỏi tiếp theo */}
+          </button>
+          <button className="btn btn-warning " onClick={() => handleNext()}>
+            Finish {/* Nút Next để chuyển đến câu hỏi tiếp theo */}
           </button>
         </div>
       </div>
-
-      <div className="right-content">count down</div>
+      <div className="right-content">count down</div>{" "}
+      {/* Hiển thị phần đếm ngược */}
     </div>
   );
 };
--- src/component.js/Users/Questions.js
+++ src/component.js/Users/Questions.js
@@ -1,32 +1,52 @@
-import _ from "lodash";
-import NTE from "../../assets/NTE.mp4";
+import _ from "lodash"; // Import thư viện lodash để sử dụng các hàm tiện ích
+import TFT from "../../assets/TFT_Fates_Header_SEO.jpg"; // Import file video từ thư mục assets
+
 
 const Question = (props) => {
-  const { data, index } = props;
+  const { data, index } = props; // Lấy dữ liệu và index từ props
   if (_.isEmpty(data)) {
-    return <></>;
+    // Kiểm tra nếu data rỗng
+    return <></>; // Trả về một phần tử rỗng nếu data rỗng
   }
+
+  const handleCheckBox = (event, aID,qId) => {
+    // console.log("check", event.target.checked);
+    console.log('>>> data props', aID, qId)
+    props.handleCheckBox2(aID,qId)
+  };
 
   return (
     <>
-      {data.image && (
+      {data.image ? ( // Kiểm tra nếu có ảnh trong data
         <div className="q-image">
-          <img src={`data:image/jpeg;base64,${data.image}`} />
+          <img src={`data:image/jpeg;base64,${data.image}`} alt={TFT} />
+        </div>
+      ) : (
+        <div className="q-image">
+          <img src={TFT} alt="ok" />
         </div>
       )}
+
       <div className="question">
-        Question {index + 1}: {data.questionDescription} ?{" "}
+        Câu hỏi {index + 1}: {data.questionDescription} ?
       </div>
 
       <div className="answer">
-        {data.answers &&
-          data.answers.length &&
+        {data.answers && // Kiểm tra nếu có câu trả lời trong data
+          data.answers.length && // Kiểm tra nếu mảng câu trả lời không rỗng
           data.answers.map((a, index) => {
+            // Lặp qua mỗi câu trả lời trong mảng
             return (
               <div key={`answer-${index}`} className="a-child">
-                <div class="form-check">
-                  <input class="form-check-input" type="checkbox" value="" />
-                  <label class="form-check-label">{a.description}</label>
+                <div className="form-check">
+                  <input
+                    className="form-check-input"
+                    type="checkbox"
+                    checked={a.isSelected}
+                    onChange={(event) => handleCheckBox(event,a.id,data.questionId)}
+                  />
+
+                  <label className="form-check-label">{a.description}</label>
                 </div>
               </div>
             );
@@ -36,4 +56,4 @@ const Question = (props) => {
   );
 };
 
-export default Question;
+export default Question; // Xuất component Question để có thể sử dụng ở nơi khác

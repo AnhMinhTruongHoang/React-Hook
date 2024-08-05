3d7105638d016a7684957666e9bd7db8f3c6dde5
AnhMinhTruongHoang <truonghoanganhminh2000@gmail.com>
2024-08-06 01:59:37 +0700

    ok

--- src/component.js/Users/ModalResult.js
+++ src/component.js/Users/ModalResult.js
@@ -1,10 +1,17 @@
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
+import { useNavigate } from "react-router-dom";
 
 const ModalResult = (props) => {
   const { show, setShow, dataModalR } = props;
+  const Navigate = useNavigate();
 
-  const handleClose = () => setShow(false);
+  const handleClose = () => {
+    setShow(false);
+  };
+  const handleClose2 = () => {
+    Navigate("/");
+  };
 
   return (
     <>
@@ -13,15 +20,21 @@ const ModalResult = (props) => {
           <Modal.Title>Your Result !</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <div>Total Question: <b style={{color: 'red'}}>{dataModalR.countTotal}</b> </div>
-          <div>Total Correct answers: <b  style={{color: 'green'}}>{dataModalR.countCorrect}</b> </div>
+          <div>
+            Total Question:{" "}
+            <b style={{ color: "red" }}>{dataModalR.countTotal}</b>{" "}
+          </div>
+          <div>
+            Total Correct answers:{" "}
+            <b style={{ color: "green" }}>{dataModalR.countCorrect}</b>{" "}
+          </div>
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
             Show answer
           </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Close
+          <Button variant="primary" onClick={handleClose2}>
+            Back
           </Button>
         </Modal.Footer>
       </Modal>
